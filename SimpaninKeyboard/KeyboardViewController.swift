34c5ee1f26fc952f1ee6ea19dd53538ff7858592
geff <754277710@qq.com>
2026-05-29 17:08:44 +0800

    优化

--- SimpaninKeyboard/KeyboardViewController.swift
+++ SimpaninKeyboard/KeyboardViewController.swift
@@ -3067,7 +3067,7 @@ final class KeyboardViewController: UIInputViewController {
             cardView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
             cardView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
             cardView.centerYAnchor.constraint(equalTo: container.centerYAnchor),
-            cardView.heightAnchor.constraint(equalToConstant: 172),
+            cardView.heightAnchor.constraint(equalToConstant: 192),
 
             stack.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
             stack.centerYAnchor.constraint(equalTo: cardView.centerYAnchor),
